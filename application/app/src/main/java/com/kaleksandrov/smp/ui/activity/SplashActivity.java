fd0c675b5254702a8f3b416a7e1afd259300754e
kaleksandrov <kiril.aleksandrov.89@gmail.com>
2015-07-11 13:42:45 +0300

    Added motion when scrolling media library.

--- application/app/src/main/java/com/kaleksandrov/smp/ui/activity/SplashActivity.java
+++ application/app/src/main/java/com/kaleksandrov/smp/ui/activity/SplashActivity.java
@@ -25,13 +25,7 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             protected Void doInBackground(Void... params) {
-                try {
-                    Thread.sleep(3 * 1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                final FairPlayerApplication app = (FairPlayerApplication) getApplicationContext();
+                FairPlayerApplication app = (FairPlayerApplication) getApplicationContext();
                 app.initResource();
                 return null;
             }
